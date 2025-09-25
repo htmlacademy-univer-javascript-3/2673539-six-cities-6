dbf949390c29e8993d5cb647ff029e134c5cedcd
lehakurshev <lehakurshev@mail.ru>
2025-09-25 19:49:04 +0500

    feat(module2-task1): Добавил страницы и компоненты из соотвецтвующего задания

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer container">
+      <a className="footer__logo-link" href="main.html">
+        <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"></img>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
